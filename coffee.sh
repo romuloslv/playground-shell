d9f6cf7b00894e4c1c597671124958347569a640
Rômulo Ferreira <romuloslv@users.noreply.github.com>
2022-09-24 19:01:51 -0300

    fix: remove test

--- coffee.sh
+++ coffee.sh
@@ -12,7 +12,6 @@ function tput_loop() {
 }
 
 function coffee() {
-    echo -e "\nAs promised, here is your coffee and your next information.\nTip: /opt/bert.rb"
     IFS='%'
     tput civis
     while [ "$(ps a | awk '{print $1}' | grep $1)" ]; do
